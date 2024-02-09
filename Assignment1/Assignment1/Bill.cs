317273ceaeaeedabcae74ed47d25d1e690667467
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2024-02-09 15:57:41 -0500

    Added Code In all Files

--- Assignment1/Assignment1/Bill.cs
+++ Assignment1/Assignment1/Bill.cs
@@ -3,9 +3,22 @@ namespace Assignment1
 {
 	public class Bill
 	{
-		public Bill()
-		{
-		}
-	}
+        private string userName;
+        private double price;
+        private int itemNumber;
+
+        public string Name { get; set; }
+        public double pricePerItem { get; set; }
+        public int numberOfItems { get; set; }
+        public double Discount { get; set; }
+
+        public Bill(string userName, double price, int itemNumber, double discount)
+        {
+            Name = userName;
+            pricePerItem = price;
+            numberOfItems = itemNumber;
+            Discount = discount;
+        }
+    }
 }
 
--- Assignment1/Assignment1/Page1.xaml.cs
+++ Assignment1/Assignment1/Page1.xaml.cs
@@ -6,4 +6,57 @@ public partial class Page1 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        string userName = NameEntry.Text;
+        int itemNumber = int.Parse(NumberOfItemsEntry.Text);
+
+        if (double.TryParse(PriceEntry.Text, out double price))
+        {
+            double discount = GetDiscount();
+            double discountedPrice = calDiscountPrice(price, itemNumber, discount);
+
+            Label1.Text = $"Customer Name : {userName}";
+            PricePerItemLabel.Text = $"Price Per Item : {price}";
+            NumberOfItemsLabel.Text = $"Number Of Items : {itemNumber}";
+            DiscountLabel.Text = $"Discount : {discount}";
+            TotalPriceLabel.Text = $"Total Price : {discountedPrice:C}";
+        }
+        else
+        {
+            Label1.Text = "Invalid price Entered";
+        }
+    }
+
+    private double GetDiscount()
+    {
+        if (percent5.IsChecked)
+            return 0.05;
+        else if (percent10.IsChecked)
+            return 0.10;
+        else if (percent15.IsChecked)
+            return 0.15;
+        else if (percent20.IsChecked)
+            return 0.20;
+        else
+            return 0.0;
+    }
+    private double calDiscountPrice(double price, double itemNumber, double discount)
+    {
+        return (itemNumber * price * 1.13) - (itemNumber * price * 1.13 * discount);
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+        NameEntry.Text = string.Empty;
+        PriceEntry.Text = string.Empty;
+        NumberOfItemsEntry.Text = string.Empty;
+        Label1.Text = string.Empty;
+        PricePerItemLabel.Text = string.Empty;
+        NumberOfItemsLabel.Text = string.Empty;
+        DiscountLabel.Text = string.Empty;
+        TotalPriceLabel.Text = string.Empty;
+    }
 }
