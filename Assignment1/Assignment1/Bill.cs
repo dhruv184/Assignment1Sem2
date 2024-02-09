d2bf9782f075d4d5be9233551aaee27d77dc3cb7
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2024-02-09 15:52:45 -0500

    Added Files

---
+++ Assignment1/Assignment1/Bill.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Assignment1
+{
+	public class Bill
+	{
+		public Bill()
+		{
+		}
+	}
+}
+
---
+++ Assignment1/Assignment1/Page1.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment1;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
